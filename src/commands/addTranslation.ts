6828172588cc51f44010c3ed4eb57a610be089b3
Stijn Rutten <stijn7rutten@hotmail.com>
2021-10-18 20:40:27 +0200

    Finish translation command

--- src/commands/addTranslation.ts
+++ src/commands/addTranslation.ts
@@ -1,16 +1,63 @@
+import { env, window, workspace } from "vscode";
+import * as fs from "fs";
+import * as jsonfile from "jsonfile";
 
-import { window } from 'vscode';
 export async function addTranslation() {
-    const result = await window.showInputBox({
-        title: 'Provide text to translate',
-        placeHolder: 'For example: Click here to go to checkout',
-        validateInput: text => {
-            console.log(text);
-            const regex = /^([a-zA-Z]+\.){2}[a-zA-Z]+$/;
-            return regex.test(text) ? 'String does not match format (Domain.MessageType.Identifier e.g. Checkout.Title.WelcomeToCheckout)' : null;
+  if (!workspace.workspaceFolders) {
+    return;
+  }
+
+  const workSpacePath = workspace.workspaceFolders[0].uri.fsPath;
+  const i18nFolderPath = `${workSpacePath}\\src\\assets\\i18n`;
+
+  const identifier = await window.showInputBox({
+    title: "Provide Transloco identifier",
+    placeHolder: "e.g. Homepage.Title.HelloWorld",
+  });
+
+  if (!identifier) {
+    return;
+  }
+
+  const text = await window.showInputBox({
+    title: "Provide text",
+    placeHolder: "e.g. Welcome to my hello world app!",
+  });
+
+  if (!identifier || !text) {
+    return;
+  }
+
+  const identifierParts = identifier?.split(".");
+
+  fs.readdir(i18nFolderPath, (err, files) => {
+    files.forEach((file) => {
+      const filePath = `${i18nFolderPath}\\${file}`;
+      jsonfile.readFile(filePath, (err, data) => {
+        if (!data) {
+          data = {};
         }
+
+        let schema = data;
+        for (let i = 0; i < identifierParts.length; i++) {
+          const elem = identifierParts[i];
+
+          if (!schema[elem]) {
+            schema[elem] = {};
+          }
+
+          if (i + 1 === identifierParts.length) {
+            schema[elem] = text;
+          } else {
+            schema = schema[elem];
+          }
+        }
+
+        jsonfile.writeFile(filePath, data, { spaces: 2 });
+      });
     });
-    if (result) {
-        window.showInformationMessage(result.toString());
-    }
+  });
+
+  env.clipboard.writeText(`{{ t('${identifier}') }}`);
+  window.showInformationMessage("Transloco identifier copied to clipboard");
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,15 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { addTranslation } from './commands/addTranslation';
+import { translateSelectedText } from './commands/translateSelectedText';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
 	const addTranslationCommand = vscode.commands.registerCommand('extension.addTranslation', addTranslation);
+
+	const translateSelectedTextCommand = vscode.commands.registerCommand('extension.translateSelectedText', translateSelectedText);
 }
 
 // this method is called when your extension is deactivated
