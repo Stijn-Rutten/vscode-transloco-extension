a99301385dc43c0a4f275df89880d380437e9969
Stijn Rutten <stijn7rutten@hotmail.com>
2021-10-18 22:07:02 +0200

    Add command to replace selected text

--- src/commands/addTranslation.ts
+++ src/commands/addTranslation.ts
@@ -1,15 +1,7 @@
 import { env, window, workspace } from "vscode";
-import * as fs from "fs";
-import * as jsonfile from "jsonfile";
+import { writeToJsonFiles } from "./write-to-json-files";
 
 export async function addTranslation() {
-  if (!workspace.workspaceFolders) {
-    return;
-  }
-
-  const workSpacePath = workspace.workspaceFolders[0].uri.fsPath;
-  const i18nFolderPath = `${workSpacePath}\\src\\assets\\i18n`;
-
   const identifier = await window.showInputBox({
     title: "Provide Transloco identifier",
     placeHolder: "e.g. Homepage.Title.HelloWorld",
@@ -28,36 +20,8 @@ export async function addTranslation() {
     return;
   }
 
-  const identifierParts = identifier?.split(".");
+  writeToJsonFiles(identifier, text);
 
-  fs.readdir(i18nFolderPath, (err, files) => {
-    files.forEach((file) => {
-      const filePath = `${i18nFolderPath}\\${file}`;
-      jsonfile.readFile(filePath, (err, data) => {
-        if (!data) {
-          data = {};
-        }
-
-        let schema = data;
-        for (let i = 0; i < identifierParts.length; i++) {
-          const elem = identifierParts[i];
-
-          if (!schema[elem]) {
-            schema[elem] = {};
-          }
-
-          if (i + 1 === identifierParts.length) {
-            schema[elem] = text;
-          } else {
-            schema = schema[elem];
-          }
-        }
-
-        jsonfile.writeFile(filePath, data, { spaces: 2 });
-      });
-    });
-  });
-
-  env.clipboard.writeText(`{{ t('${identifier}') }}`);
+  env.clipboard.writeText(`t('${identifier}')`);
   window.showInformationMessage("Transloco identifier copied to clipboard");
 }
--- src/commands/translateSelectedText.ts
+++ src/commands/translateSelectedText.ts
@@ -1,3 +1,30 @@
+import { window } from "vscode";
+import { writeToJsonFiles } from "./write-to-json-files";
+
 export async function translateSelectedText() {
-    
+  const identifier = await window.showInputBox({
+    title: "Provide Transloco identifier",
+    placeHolder: "e.g. Homepage.Title.HelloWorld",
+  });
+
+  const activeTextEditor = window.activeTextEditor;
+  if (!activeTextEditor) {
+    return;
+  }
+
+  const currentSelection = activeTextEditor.selection;
+  if (!currentSelection) {
+    return;
+  }
+
+  const text = activeTextEditor.document.getText(currentSelection);
+  if (!identifier || !text) {
+    return;
+  }
+
+  writeToJsonFiles(identifier, text);
+
+  activeTextEditor.edit((editBuilder) => {
+    editBuilder.replace(currentSelection, `t('${identifier}')`);
+  });
 }
